49c8f4a1c63f414ccb79bbb702b4bb746ee4e7ce
Nestor07 <nfcuellar1@misena.edu.co>
2021-01-05 17:33:11 -0500

    Variables y tipos de datos

---
+++ tiposDatos.py
@@ -0,0 +1,6 @@
+x = 5     #--> Variable contiene un entero lo cual hace que la variable sea tipo entero
+y = 10.1  #--> Variable contiene un valor decimal o flotante lo cual hace que la variable sea tipo decimal o flotante
+z = True  #--> Variable de tipo boolean
+w = False #--> Variable de tipo boolean
+a = "cadena" #--> Variable de tipo String
+b = None #--> Variable sin un valor asignado
---
+++ variables.py
@@ -0,0 +1,10 @@
+x = 3
+y = 5
+z = x + y
+
+print("El valor de la suma de las variables es: " + str(z) + ", Muchas gracias!")
+
+#Estos comandos se usan para validar la ubicacion en memoria y que tipo de dato es la variable
+
+id(x)
+type(x)
